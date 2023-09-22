eab1d6934ec2928d20986133e1c7ee9e2d61fee5
Cocoandjay-em <64985694+Cocoandjay-em@users.noreply.github.com>
2023-09-22 17:54:12 +0200

    Cleanup Menus

--- resources/views/layout/partials/sidebar-layout/header/_navbar.blade.php
+++ resources/views/layout/partials/sidebar-layout/header/_navbar.blade.php
@@ -1,33 +1,5 @@
 <!--begin::Navbar-->
 <div class="app-navbar flex-shrink-0">
-    <!--begin::Search-->
-    <div class="app-navbar-item align-items-stretch ms-1 ms-md-3">
-        @include(config('settings.KT_THEME_LAYOUT_DIR').'/partials/sidebar-layout/search/_dropdown')
-    </div>
-    <!--end::Search-->
-    <!--begin::Activities-->
-	<div class="app-navbar-item ms-1 ms-md-4">
-        <!--begin::Drawer toggle-->
-		<div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px" id="kt_activities_toggle">{!! getIcon('messages', 'fs-2') !!}</div>
-        <!--end::Drawer toggle-->
-    </div>
-    <!--end::Activities-->
-    <!--begin::Notifications-->
-	<div class="app-navbar-item ms-1 ms-md-4">
-        <!--begin::Menu- wrapper-->
-		<div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end" id="kt_menu_item_wow">{!! getIcon('notification-status', 'fs-2') !!}</div>
-        @include('partials/menus/_notifications-menu')
-        <!--end::Menu wrapper-->
-    </div>
-    <!--end::Notifications-->
-    <!--begin::My apps links-->
-	<div class="app-navbar-item ms-1 ms-md-4">
-        <!--begin::Menu wrapper-->
-		<div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">{!! getIcon('element-11', 'fs-2') !!}</div>
-        @include('partials/menus/_my-apps-menu')
-        <!--end::Menu wrapper-->
-    </div>
-    <!--end::My apps links-->
     <!--begin::User menu-->
 	<div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle">
         <!--begin::Menu wrapper-->

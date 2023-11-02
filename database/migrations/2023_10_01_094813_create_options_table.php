17ab238243e825855c13c680a8535a7b0fe9a5f5
Cocoandjay-em <64985694+Cocoandjay-em@users.noreply.github.com>
2023-11-03 00:57:12 +0100

    Consolidated Options

--- database/migrations/2023_10_01_094813_create_options_table.php
+++ database/migrations/2023_10_01_094813_create_options_table.php
@@ -15,10 +15,18 @@ class CreateOptionsTable extends Migration
             $table->id();
             $table->string('name');
             $table->integer('position')->unsigned();
-            $table->enum('type', ['yes_no', 'check', 'radio']);
+            $table->enum('type', ['yes_no', 'check', 'radio', 'number', 'dropdown', 'logic']);
             $table->text('values')->nullable(); // Use a text column to store | separated values
+            $table->text('options_ids')->nullable();
+            $table->text('options_values')->nullable();
+            $table->string('logic')->nullable();
+            $table->string('description')->nullable();
+            $table->text('season_ids')->nullable();
+            $table->text('venue_ids')->nullable();
+            $table->string('default_value')->nullable();
+            $table->decimal('vat', 8, 2)->nullable();
+            $table->boolean('always_included')->default(false);
             $table->timestamps();
-
             // Add an index on the "position" column for performance
             $table->index('position');
         });

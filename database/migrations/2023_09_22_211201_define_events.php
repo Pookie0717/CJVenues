ef6b383c93d07d438c022a2b592cd29f3c6f1318
Cocoandjay-em <64985694+Cocoandjay-em@users.noreply.github.com>
2023-11-03 00:52:40 +0100

    Consolidating migrations

--- database/migrations/2023_09_22_211201_define_events.php
+++ database/migrations/2023_09_22_211201_define_events.php
@@ -13,17 +13,30 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('events_type', function (Blueprint $table) {
+        Schema::create('event_types', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('event_name');
             $table->string('typical_seating');
             $table->string('duration_type');
-            $table->string('max_duration');
             $table->string('min_duration');
+            $table->string('max_duration');
             $table->string('time_setup');
             $table->string('time_cleaningup');
             $table->unsignedBigInteger('season_id');  // Foreign key to associate with a period
-            $table->string('availability');
+            $table->integer('min_people')->nullable();
+            $table->integer('max_people')->nullable();
+
+            // Add 'description' column
+            $table->text('description')->nullable();
+
+            // Change 'season_id' column to 'seasons' (array)
+            //$table->dropColumn('season_id');
+            $table->json('seasons')->nullable();
+
+            // Add 'opening_time' and 'closing_time' columns
+            $table->time('opening_time')->nullable();
+            $table->time('closing_time')->nullable();
             $table->timestamps();
         });
     }
@@ -36,6 +49,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('events_type');
+        Schema::dropIfExists('event_types');
     }
 };

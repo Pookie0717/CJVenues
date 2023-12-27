de52c3bea2b395ecd078fd5b8bd468dd2bb802a4
Cocoandjay-em <64985694+Cocoandjay-em@users.noreply.github.com>
2023-12-27 11:58:29 -0300

    Reverted Landing Page Commit

--- routes/web.php
+++ routes/web.php
@@ -29,83 +29,75 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware(['auth', 'verified'])->group(function () {
 
-// Group routes for dashboard.cjvenues.com
-Route::domain('dashboard.cjvenues.com')->group(function () {
-    Route::middleware(['auth', 'verified'])->group(function () {
-        Route::get('setlocale/{locale}', [LocalizationController::class, 'setLocale'])->name('setlocale');
+Route::get('setlocale/{locale}', 'App\Http\Controllers\LocalizationController@setLocale')->name('setlocale');
 
-        Route::get('/', [DashboardController::class, 'index']);
-        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/', [DashboardController::class, 'index']);
 
-        Route::name('user-management.')->group(function () {
-            Route::resource('/user-management/users', UserManagementController::class);
-            Route::resource('/user-management/roles', RoleManagementController::class);
-            Route::resource('/user-management/permissions', PermissionManagementController::class);
-        });
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-        // Routes for Venues Management
-        Route::resource('/venues-management/venues', VenuesManagementController::class)->names([
-            'index' => 'venues', // Rename the index route
-        ]);
-
-        // Routes for Areas Management
-        Route::resource('/venues-management/areas', AreasManagementController::class)->names([
-            'index' => 'areas', // Rename the index route
-        ]);
-
-        Route::resource('/contacts', ContactsController::class)->names([
-            'index' => 'contacts'
-        ]);
-
-        Route::resource('/bookings', BookingsController::class)->names([
-            'index' => 'bookings'
-        ]);
-
-        Route::resource('/prices', PricesController::class)->names([
-            'index' => 'prices'
-        ]);
-
-        Route::resource('/seasons', SeasonsController::class)->names([
-            'index' => 'seasons'
-        ]);
-
-        Route::resource('/options', OptionsController::class)->names([
-            'index' => 'options'
-        ]);
-
-        Route::resource('/event-types', EventTypesController::class)->names([
-            'index' => 'event-types'
-        ]);
-
-        Route::resource('/organizations', TenantController::class)->names([
-            'index' => 'organizations'
-        ]);
-
-        Route::resource('/quotes', QuotesController::class)->names([
-            'index' => 'quotes'
-        ]);
-
-        Route::get('quotes/{quote}', [QuotesController::class, 'show'])->name('quotes.show');
-        Route::post('/quotes/{id}/book', [QuotesController::class, 'book'])->name('quotes.book');
+    Route::name('user-management.')->group(function () {
+        Route::resource('/user-management/users', UserManagementController::class);
+        Route::resource('/user-management/roles', RoleManagementController::class);
+        Route::resource('/user-management/permissions', PermissionManagementController::class);
     });
 
-    Route::get('/error', function () {
-        abort(500);
-    });
+    // Routes for Venues Management
+    Route::resource('/venues-management/venues', VenuesManagementController::class)->names([
+        'index' => 'venues', // Rename the index route
+    ]);
 
-    Route::get('/auth/redirect/{provider}', [SocialiteController::class, 'redirect']);
-    Route::get('quotesLink/{hashedId}', [QuotesController::class, 'showPublic'])->name('quotes.showPublic')->middleware('web');
-    Route::post('/set-tenant', [TenantController::class, 'setTenant'])->name('set-tenant');
+    // Routes for Areas Management
+    Route::resource('/venues-management/areas', AreasManagementController::class)->names([
+        'index' => 'areas', // Rename the index route
+    ]);
+
+    Route::resource('/contacts', ContactsController::class)->names([
+        'index' => 'contacts'
+    ]);
+
+    Route::resource('/bookings', BookingsController::class)->names([
+        'index' => 'bookings'
+    ]);
+
+    Route::resource('/prices', PricesController::class)->names([
+        'index' => 'prices'
+    ]);
+
+    Route::resource('/seasons', SeasonsController::class)->names([
+        'index' => 'seasons'
+    ]);
+
+    Route::resource('/options', OptionsController::class)->names([
+        'index' => 'options'
+    ]);
+
+    Route::resource('/event-types', EventTypesController::class)->names([
+        'index' => 'event-types'
+    ]);
+
+    Route::resource('/organizations', TenantController::class)->names([
+        'index' => 'organizations'
+    ]);
+
+    Route::resource('/quotes', QuotesController::class)->names([
+        'index' => 'quotes'
+    ]);
+
+    Route::get('quotes/{quote}', [QuotesController::class, 'show'])->name('quotes.show');
+    Route::post('/quotes/{id}/book', [QuotesController::class, 'book'])->name('quotes.book');
+
 });
 
-// Add the new route for cjvenues.com
-Route::domain('cjvenues.com')->group(function () {
-    Route::get('/', function () {
-        // Return the view for your new homepage
-        return view('welcome_cjvenues');
-    });
+Route::get('/error', function () {
+    abort(500);
 });
 
-// Include the auth routes file
+Route::get('/auth/redirect/{provider}', [SocialiteController::class, 'redirect']);
+
+Route::get('quotesLink/{hashedId}', [QuotesController::class, 'showPublic'])->name('quotes.showPublic')->middleware('web');
+
+Route::post('/set-tenant', 'App\Http\Controllers\TenantController@setTenant')->name('set-tenant');
+
 require __DIR__ . '/auth.php';

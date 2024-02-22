f0dc6b15bb5a04f84394fbaceb1ed56c0ed811f6
cappedapollo <cappedapollo@gmail.com>
2024-02-22 08:44:02 -0500

    fix case insensitive

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\DataTables\staffDataTable;
+use App\DataTables\StaffDataTable;
 
 class StaffController extends Controller
 {
